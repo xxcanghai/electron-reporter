593365f3016588919fa09db7878cf81a9635f249
wangtao2012 <wangtao@getnada.com>
2017-04-13 12:33:28 +0800

    Update Reporter.js
    Please consider always use async since `fs-grateful` is buggy with `fs.statSync` and it blocks the entire process.
    我用Windows跑代码的时候fs.statSync总是报错EPERM。搜了下应该是fs-promise-extra里调用的graceful-fs里的statSync的一个bug。

--- src/Reporter.js
+++ src/Reporter.js
@@ -700,7 +700,7 @@ class Reporter {
       if (files2Report.length === 1) {
         let file = files2Report[0]
         // 文件大小 小于50k 走字符串上报流程
-        let {size} = fs.statSync(file.filePath)
+        let {size} = await fs.statAsync(file.filePath)
         if (size < 1024 * 50) {
           return this._push2LogQueue(file)
         } else { // 否则走文件上传流程
